650df806d457c059f96c929670929dbb3d471974
Amelia Miner <ameliamminer@gmail.com>
2025-02-17 12:53:59 -0800

    moved from google's weird cloud function server thing to a normal flask app

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ MAX_FILESIZE_B = 1000 * MAX_FILESIZE_KB
 app = Flask('ansifier-cloud')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
     """
     Returns:
@@ -178,5 +178,4 @@ def serve_UI():
     return ret
 
 if __name__ == '__main__':
-    print('running')
     app.run(debug=True)
