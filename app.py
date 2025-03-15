8cac5e49f0cf6c68351c0fafcb371e707e507e40
Amelia Miner <ameliamminer@gmail.com>
2025-03-14 23:58:07 -0700

    cloud vision image moderation works in dev env, form has submit-to-library checkbox

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import requests
 import validators
 
 from ansifier import ansify
+from google.cloud import vision
 
 from flask import Flask, request, render_template
 
@@ -23,11 +24,14 @@ MAX_FILESIZE_MB = 5
 MAX_FILESIZE_KB = 1000 * MAX_FILESIZE_MB
 MAX_FILESIZE_B = 1000 * MAX_FILESIZE_KB
 app = Flask('ansifier-cloud')
+debug = os.environ.get('ANSIFIER_CLOUD_DEBUG', None)
 #client = google.cloud.logging.Client()
 #client.setup_logging()
+
 def log_info(message):
     app.logger.info(message)
     logging.info(message)
+    print(message)
 
 
 @app.route('/', methods=['GET'])
@@ -70,7 +74,7 @@ def file_flow(received_file, request):
         message = save_image_werkzeug(received_file)
         message = process_imagefile(request, "the file you uploaded")
     except Exception as e:
-        message = message + "\n" + str(e)
+        message = message + f"\n{str(e)}" if debug else ""
     return message
 
 
@@ -87,9 +91,44 @@ def url_flow(image_url, request):
         message = save_image_bytes(message)
         message = process_imagefile(request, image_url)
     except Exception as e:
-        message = message + "\n" + str(e)
+        message = message + f"\n{str(e)}" if debug else ""
     return message
 
+
+def moderate_imagefile():
+    """
+    runs the data stored at IMAGE_FILEPATH through Google's safesearch ML model,
+    raises an exception if the image is deemed inappropriate
+    :param image_url: str, only used for logging
+    :return: None
+    """
+    log_info("moderating locally stored image file")
+    image = vision.Image()
+    client = vision.ImageAnnotatorClient()
+
+    with open(IMAGE_FILEPATH, 'rb') as rf:
+        image.content = rf.read() # TODO reading file twice, once to moderate and once to ansify...
+    response = client.safe_search_detection(image=image)
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+
+    safe = response.safe_search_annotation
+    # Names of likelihood from google.cloud.vision.enums
+    #"UNKNOWN",  # 0
+    #"VERY_UNLIKELY",  # 1
+    #"UNLIKELY",  # 2
+    #"POSSIBLE",  # 3
+    #"LIKELY",  # 4
+    #"VERY_LIKELY",  # 5
+    #safe.adult, medical, spoofed, violence, racy
+    if safe.violence > 3:
+        raise Exception("Detected violence in image - service refused")
+    # TODO anything else to restrict?
+
+    
 
 def process_imagefile(request, image_url):
     """
@@ -97,10 +136,13 @@ def process_imagefile(request, image_url):
     :param image_url: str, only used for logging
     :return: str, see main
     """
-    log_info(f"ansifying downloaded copy of {image_url}")
+    log_info(f"processing downloaded copy of {image_url}")
+
+    moderate_imagefile()
 
     format_raw = request.form.get('format')
     characters_raw = request.form.get('characters')
+    library_raw = request.form.get('library')
 
     if format_raw is None:
         format_raw = 'ansi-escaped'
@@ -121,6 +163,10 @@ def process_imagefile(request, image_url):
     width = validate_dim(request.form.get('width'))
     height = validate_dim(request.form.get('height'))
 
+    print(library)
+    if library == 'true':
+        pass
+        # TODO submit to library if requested
     return ansify(IMAGE_FILEPATH, output_format=format_raw, chars=characters,
                     height=height, width=width)[0]
 
@@ -128,50 +174,48 @@ def process_imagefile(request, image_url):
 def save_image_werkzeug(image):
     """ saves an image to disk from a werkzeug file object """
     log_info("werkzeug-saving image to file...")
-    message = r"failed to process image ¯\_(ツ)_/¯"
+
     image.seek(0)
     file_size = len(image.read())
     log_info(f"received {file_size} byte image to save")
     if file_size > MAX_FILESIZE_B:
         raise ValueError(f"File is ~{file_size/1e6} MB, must not exceed {MAX_FILESIZE_MB} MB")
+
     image.seek(0)
     image.save(IMAGE_FILEPATH)  # TODO may be reading into memory twice here
     saved_size = os.path.getsize(IMAGE_FILEPATH)
     log_info(f"saved {saved_size} bytes to {IMAGE_FILEPATH}")
 
-    message = f"Image saved to {IMAGE_FILEPATH}"
-    return message
+    return f"image saved to {IMAGE_FILEPATH}"
 
 
 def save_image_bytes(content):
     log_info("wb-saving image to file...")
-    message = r"failed to process image ¯\_(ツ)_/¯"
     with open(IMAGE_FILEPATH, "wb") as wf:
         wf.write(content)
 
-    message = f"image saved to {IMAGE_FILEPATH}"
-    return message
+    return f"image saved to {IMAGE_FILEPATH}"
 
 
 def download_url(url):
     log_info(f"downloading image from {url}")
-    message = r"failed to download image ¯\_(ツ)_/¯"
     s = requests.session()
     head_raw = s.head(url)
+
     if head_raw.status_code < 200 or head_raw.status_code > 299:
-        raise ValueError(f"image url returned code {head_raw.status_code}")
+        raise ValueError(f"image url returned code {head_raw.status_code} - try downloading the "
+                         "image to your device, then uploading it instead of linking to it.")
+
     size = int(head_raw.headers.get("Content-Length", 0))
     if size > MAX_FILESIZE_B:
         raise ValueError(f"File must not exceed {MAX_FILESIZE_MB} MB")
 
     content_raw = s.get(url, timeout=10)
-    message = content_raw.content
-    return message
+    return content_raw.content
 
 
 def validate_url(url):
     log_info(f"validating {url}")
-    message = r"no valid image URL provided ¯\_(ツ)_/¯"
 
     if not validators.url(url):
         raise ValueError("valid URL must be supplied")
@@ -180,9 +224,8 @@ def validate_url(url):
     if not any(map(lambda ex: url.endswith(ex), FILE_EXTENSIONS)):
         raise ValueError(f"file type must be one of {FILE_EXTENSIONS}")
 
-    message = f"{url} validated"
-    return message
+    return f"{url} validated"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=debug)
