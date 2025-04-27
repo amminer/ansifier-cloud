46e75c9b661523594201c5d44ef7ada8d6e32747
Amelia Miner <ameliamminer@gmail.com>
2025-04-26 18:00:14 -0700

    separate prod and dev backends

--- data_model/gcp_mysql_model.py
+++ data_model/gcp_mysql_model.py
@@ -9,6 +9,7 @@ class GcpMySQLDBSession(BaseDBSession):
     Interfaces with GCP MySQL cloud db according to the env vars below
     """
     def __init__(self):
+        db_env = environ.get("DB_ENV")  # "prod" or "test"
         db_host = environ.get("INSTANCE_HOST")  # an IP address
         db_user = environ.get("DB_USER")  # e.g. 'my-db-user'
         db_pass = environ.get("DB_PASS")  # e.g. 'my-db-password'
@@ -19,19 +20,29 @@ class GcpMySQLDBSession(BaseDBSession):
 
         # https://cloud.google.com/sql/docs/mysql/samples/cloud-sql-mysql-sqlalchemy-connect-tcp-sslcerts
         # this cert will expire in 2035
-        ssl_args = {
-            'ssl_ca': 'static/server-ca.pem',
-            # client cert & key would go here under ssl_cert & ssl_key
-        }
 
-        engine = sqlalchemy.create_engine(
-            sqlalchemy.engine.url.URL.create(
-                drivername="mysql+pymysql",
-                username=db_user,
-                password=db_pass,
-                host=db_host,
-                port=db_port,
-                database=db_name),
-            connect_args=ssl_args
-        )
+        if db_env == 'prod':  # internal gcp access only, no certs given
+            engine = sqlalchemy.create_engine(
+                sqlalchemy.engine.url.URL.create(
+                    drivername="mysql+pymysql",
+                    username=db_user,
+                    password=db_pass,
+                    host=db_host,
+                    port=db_port,
+                    database=db_name))
+        else:
+            ssl_args = {
+                'ssl_ca': 'static/test-server-ca.pem',
+                # client cert & key would go here under ssl_cert & ssl_key
+            }
+            engine = sqlalchemy.create_engine(
+                sqlalchemy.engine.url.URL.create(
+                    drivername="mysql+pymysql",
+                    username=db_user,
+                    password=db_pass,
+                    host=db_host,
+                    port=db_port,
+                    database=db_name),
+                connect_args=ssl_args)
+
         super().__init__(engine)  # opens self.session for queries
