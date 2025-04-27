34ccc9203b01a6b019e8a09a4b8106664ddfbee1
Amelia Miner <ameliamminer@gmail.com>
2025-04-26 18:55:39 -0700

    figured it out - I was confused about gcp UI, go figure. Added prod db ssl ca

--- data_model/gcp_mysql_model.py
+++ data_model/gcp_mysql_model.py
@@ -15,34 +15,30 @@ class GcpMySQLDBSession(BaseDBSession):
         db_pass = environ.get("DB_PASS")  # e.g. 'my-db-password'
         db_name = environ.get("DB_NAME")  # e.g. 'my-database'
         db_port = environ.get("DB_PORT")  # e.g. 3306
+
         if db_port is None:
             db_port = 3306
+
+        if db_env == 'prod':
+            ssl_ca ='static/prod-server-ca.pem' 
+        else:
+            ssl_ca = 'static/test-server-ca.pem' 
 
         # https://cloud.google.com/sql/docs/mysql/samples/cloud-sql-mysql-sqlalchemy-connect-tcp-sslcerts
         # this cert will expire in 2035
 
-        if db_env == 'prod':  # internal gcp access only, no certs given
-            engine = sqlalchemy.create_engine(
-                sqlalchemy.engine.url.URL.create(
-                    drivername="mysql+pymysql",
-                    username=db_user,
-                    password=db_pass,
-                    host=db_host,
-                    port=db_port,
-                    database=db_name))
-        else:
-            ssl_args = {
-                'ssl_ca': 'static/test-server-ca.pem',
-                # client cert & key would go here under ssl_cert & ssl_key
-            }
-            engine = sqlalchemy.create_engine(
-                sqlalchemy.engine.url.URL.create(
-                    drivername="mysql+pymysql",
-                    username=db_user,
-                    password=db_pass,
-                    host=db_host,
-                    port=db_port,
-                    database=db_name),
-                connect_args=ssl_args)
+        ssl_args = {
+            'ssl_ca': ssl_ca,
+            # client cert & key would go here under ssl_cert & ssl_key
+        }
+        engine = sqlalchemy.create_engine(
+            sqlalchemy.engine.url.URL.create(
+                drivername="mysql+pymysql",
+                username=db_user,
+                password=db_pass,
+                host=db_host,
+                port=db_port,
+                database=db_name),
+            connect_args=ssl_args)
 
         super().__init__(engine)  # opens self.session for queries
